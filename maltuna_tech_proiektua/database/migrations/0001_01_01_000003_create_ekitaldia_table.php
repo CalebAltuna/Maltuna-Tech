e998df13abb8c50f41f10c4f41594839242baca4
MALTUNA\web25lfranco <lukenfranco@gmail.com>
2025-10-21 11:46:07 +0200

    datubasea igota

---
+++ maltuna_tech_proiektua/database/migrations/0001_01_01_000003_create_ekitaldia_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('ekitaldiak', function (Blueprint $table) {
+            // Primary Key
+            $table->id('id_ekitaldiak');
+
+            // Columnas normales
+            $table->string('izenburu');
+            $table->text('deskripzioa')->nullable(); // supongo que Deskripzioa es texto
+            $table->integer('aforo');
+            $table->integer('aforo_libre')->nullable();
+            $table->string('egoera')->nullable();
+
+            // Foreign Key
+            $table->foreignId('id_sortzaile')->constrained('admin')->onDelete('cascade');
+
+            // Fechas y otros
+            $table->date('data_erosketa')->nullable();
+            $table->integer('prezio_sarrera')->nullable();
+            $table->string('irudia')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ekitaldiak');
+    }
+};
